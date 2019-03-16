3da7a16c14c6476554c47bedddf1ccc698087ce8
unknown <almeshari93@gmail.com>
2019-03-16 23:02:52 +0300

    display three messages, as shown in Listing 1.2.

---
+++ A Simple Java Program/welcome.java
@@ -0,0 +1,6 @@
+public class welcome {
+    public static void main(String[] args) {
+        // Desplay messige Welcome to Java ! on the console 
+        System.out.println("Welcome to Java");
+    }
+}
