f3eca27063e6e8f61e050eed2a6e1cadb6bbeb6e
unknown <almeshari93@gmail.com>
2019-03-17 01:30:25 +0300

    1.5 (Compute expressions)

---
+++ Chapter I/EXERCISES/Ch1Ex5.java
@@ -0,0 +1,15 @@
+package EXERCISES;
+
+/**
+ * 1.5 (Compute expressions) Write a program that displays the result of
+ * 
+ *             9.5 * 4.5 - 2.5 * 3 
+ *            _____________________
+ *                 45.5 - 3.5
+ */ 
+
+ public class Ch1Ex5 {
+     public static void main(String[] args) {
+         System.out.println(((9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5)));
+     }
+ }
