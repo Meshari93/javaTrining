c63bb01644a99f6469f71e899aa380ae12845cec
unknown <almeshari93@gmail.com>
2019-03-17 00:53:29 +0300

    Exercises: 1.1

---
+++ Chapter I/EXERCISES/Ch1Ex1.java
@@ -0,0 +1,12 @@
+/*
+**
+*  1.1  (Display three messages) Write a program that displays Welcome to Java,
+*  Welcome to Computer Science, and Programming is fun.
+*/
+public class Ch1Ex1 {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to computer Science");
+        System.out.println("Progaming is fun");
+    }
+}
